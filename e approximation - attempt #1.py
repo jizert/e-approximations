157168417452cbd167f3a33e6776d9cd38f0b146
jizert <37279170+jizert@users.noreply.github.com>
2024-07-28 19:58:38 -0400

    Add files via upload

---
+++ e approximation - attempt #1.py
@@ -0,0 +1,14 @@
+# This is the first script I have made completely by myself, without a tutorial, and from an original idea.
+
+import math
+
+
+accuracy  = int(input("What level of accuracy would you like your approximation?: "))
+e = math.e 
+
+def sum_e_accuracy(n):
+    total_sum = 1.0
+    
+
+margin_of_error = e - sum_e_accuracy(accuracy)
+print("The margin of error is: " + str(margin_of_error))
